01a6ad92376e8b0331f8d8a4a5c3c291aa1db77c
Kiran.Rana <Kiran.Rana@NAL23>
2018-06-06 13:39:16 +0530

    1 filed added and one file modified by kiran

--- src/MainClass.java
+++ src/MainClass.java
@@ -3,5 +3,6 @@ public class MainClass {
 	public static void main(String[] args) {
 		System.out.println("hello kiran");
 		System.out.println("This file has been changed.");
+		System.out.println("hello Kirpal...");
 	}
 }
